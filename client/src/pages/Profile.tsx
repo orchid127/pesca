17c03dd5ad3b5e08f21f4a2632b254f633d3bb72
orchid127 <ndougakimberley@gmail.com>
2026-03-21 18:14:01 +0100

    feat : styling profile page

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -12,15 +12,22 @@ function Profile() {
     }
 
     return (
-        <div className="p-5">
+        <div className="flex flex-col p-5 max-h-screen">
             <Navbar onOpenSettings={openPopupForm} />
-            <div className="flex flex-col lg:flex-row gap-5">
-                <div className="border-[#F7C0DD] border-[2px] rounded-[10px]">
-                    <img src={pfp} alt="user pfp" className="w-30" />
-                    <p>@username</p>
+            <div className="flex flex-1 flex-col lg:flex-row gap-5">
+                <div className="flex flex-row lg:items-center lg:flex-col lg:w-[25rem] bg-[#F8C2DF]/20 border-[#EA5DA9] border-[2px] rounded-[10px] p-5 gap-5">
+                    <div className="flex-1 w-full rounded-[10px]">
+                        <img src={pfp} alt="user pfp" className="object-cover w-full h-full rounded-[10px]" />
+                    </div>
+                    <div className="flex-1 border-[#F7C0DD] border-[2px] rounded-[10px] p-5 w-full">
+                        <p className="font-kiwisoda text-[#3F7BD4] text-[2rem] md:text-[3rem]">@username</p>
+                        <p className="font-arialnarrow text-[#3F7BD4] text-[1.5rem]">statistics coming soon...</p>
+                    </div>
                 </div>
-                <div className="border-[#F7C0DD] border-[2px] rounded-[10px]">
-                    <h1 className="text-[2rem] text-[#EA5DA9] underline">Your sessions</h1>
+                <div className="flex-1 border-[#F7C0DD] border-[2px] rounded-[10px] p-5">
+                    <h1 className="font-kiwisoda text-[#EA5DA9] text-[4rem] lg:text-[6rem]">your sessions</h1>
+                    <p className="font-arialnarrow text-[#EA5DA9] text-[1.5rem]">logs coming soon...</p>
+                    <SessionList />
                 </div>
             </div>
         </div>
